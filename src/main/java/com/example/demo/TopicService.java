6bb5667136366da128b48f69cdac3db32502b496
vijayab015 <vijayab015@gmail.com>
2022-02-26 16:49:53 +0530

    first commit

---
+++ src/main/java/com/example/demo/TopicService.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	@Autowired
+	private TopicRepository topicrep;
+	
+
+	public List<Topic> getAllTopics() {
+		List<Topic> topics = new ArrayList<Topic>();
+		topicrep.findAll().forEach(topics::add);
+		return topics;
+
+	}
+
+	public Topic getTopic(String id) {
+		return topicrep.findOne(id);
+		//topicrep.
+	}
+
+	public void addTopic(Topic topic) {
+		
+
+		topicrep.save(topic);
+		
+	}
+
+	public void updateTopi(String id, Topic topic) {
+		topicrep.save(topic);
+
+	}
+
+	public void deletTopic(String id) {
+		topicrep.delete(id);
+	}
+
+}
